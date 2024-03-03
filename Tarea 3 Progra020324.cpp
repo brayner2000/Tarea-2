9caf27275dd55cd296c47bef7a67c073c06581a7
brayner2000 <bgarciaq5@miumg.edu.gt>
2024-03-02 19:37:32 -0600

    primer_commit

---
+++ Tarea 3 Progra020324.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main(){
+	
+	//i++, i+=1, i=i+1
+	int contar =0;
+	for(int i=10;i>10;i--){ // 0 hata 9
+		cout<<i<<endl;
+		contar++;
+		
+	}
+	cout<<"ciclos: "<<contar<<endl;
+	
+	
+	
+	system ("pause");
+}
